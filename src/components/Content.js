f4fe48fb8eac2f25ba0d968377eeb7e6bbc9b0a6
unknown <furkanakinmfa@gmail.com>
2023-09-24 15:45:38 +0300

    edit list place

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,37 +1,20 @@
 import React from 'react'
+import TodoList from './TodoList'
+import ContentFooter from './ContentFooter'
 
 function Content() {
   return (
-	<section classNameName="main">
-		<input className="toggle-all" type="checkbox" />
-		<label htmlfor="toggle-all">
-			Mark all as complete
-		</label>
+	<>
+		<section className="main">
+			<input className="toggle-all" type="checkbox" />
+			<label htmlFor="toggle-all">
+				Mark all as complete
+			</label>
+			<TodoList />
+		</section>
 
-		<ul className="todo-list">
-			<li className="completed">
-				<div className="view">
-					<input className="toggle" type="checkbox" />
-					<label>Learn JavaScript</label>
-					<button className="destroy"></button>
-				</div>
-			</li>
-			<li>
-				<div className="view">
-					<input className="toggle" type="checkbox" />
-					<label>Learn React</label>
-					<button className="destroy"></button>
-				</div>
-			</li>
-			<li>
-				<div className="view">
-					<input className="toggle" type="checkbox" />
-					<label>Have a life!</label>
-					<button className="destroy"></button>
-				</div>
-			</li>
-		</ul>
-	</section>
+		<ContentFooter />
+	</>
       )
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-<footer class="info">
+<footer className="info">
 	<p>Click to edit a todo</p>
 	<p>Created by <a href="https://www.linkedin.com/in/mfakin/">Furkan Akın</a></p>
 	<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
